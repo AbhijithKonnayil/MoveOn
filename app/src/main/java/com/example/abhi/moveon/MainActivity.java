1a4dd19eb4ebb6cb05e8830ed3fe6a51fc82ae8b
Abhijith K <iamabhijith.k@gmail.com>
2019-05-13 19:44:24 +0530

    login with firebase implemented

--- app/src/main/java/com/example/abhi/moveon/MainActivity.java
+++ app/src/main/java/com/example/abhi/moveon/MainActivity.java
@@ -3,21 +3,38 @@ package com.example.abhi.moveon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnRider,btnMechanic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMechanic = (Button)findViewById(R.id.mec_btn);
+        btnRider = (Button)findViewById(R.id.rider_btn);
+
+        btnRider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RiderLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        btnMechanic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MechanicLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 
-    public void riderLogin(){
-        startActivity(new Intent(this,RiderLoginActivity.class));
-    }
-
-
-    public void mechanicLogin(){
-        startActivity(new Intent(this,MechanicLoginActivity.class));
-    }
 }
